431e61da166fa7d253558a78047071d35c00318d
moulini20priyanka <moulinisrinivasan@gmail.com>
2026-04-25 17:46:03 +0530

    jwt-auth

--- frontend/pragatimitra/src/main.jsx
+++ frontend/pragatimitra/src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import router from './router'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import router from "./router";
+import { AuthProvider } from "./store/AuthContext";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
