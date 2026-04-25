6b69423ffab4a2874d48ebed9401dde268a01fdc
likesh50 <like22050.it@rmkec.ac.in>
2026-04-26 01:35:50 +0530

    department module update

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://yourfrontend.com"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
 );
