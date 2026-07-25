aa8e248729e704a5ea33a39efb68f42e662a9203
Krish Mehta <krish.mehta.0105@gmail.com>
2026-07-25 11:25:06 +0530

    Delete check_func.js

--- backend/check_func.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const p = new Pool({
-  user: 'postgres', host: 'localhost', database: 'PragatiMitra', password: 'krish@1may', port: 5432
-});
-
-p.query("SELECT proname, pg_get_function_arguments(oid) FROM pg_proc WHERE proname = 'stamp_template_to_report'")
-  .then(r => console.table(r.rows))
-  .catch(e => console.error(e))
-  .finally(() => p.end());
